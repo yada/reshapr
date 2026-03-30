459bdadd9b1c4ed69aec9ef086ff58cc1a47640a
Laurent Broudoux <laurent.broudoux@gmail.com>
2026-03-30 12:08:49 +0200

    chore: #44 Use registry.reshapr.io to access images Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- install/start-proxy.sh
+++ install/start-proxy.sh
@@ -4,4 +4,4 @@ docker run -it --rm -p 7777:7777 -e RESHAPR_CTRL_HOST=host.docker.internal \
   -e RESHAPR_CTRL_PORT=5555 \
   -e RESHAPR_CTRL_TOKEN=reshapr-my-super-secret-token-xyz \
   --add-host=host.docker.internal:host-gateway \
-  quay.io/reshapr/reshapr-proxy:nightly
+  registry.reshapr.io/reshapr/reshapr-proxy:nightly
